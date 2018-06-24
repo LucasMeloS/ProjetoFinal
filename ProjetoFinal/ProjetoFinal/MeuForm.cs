1f8efb95dad15bc2a7b2d62b6416b899c9fd1d4a
LuCaS dE MeLooo <lu_lucasmelo@hotmail.com>
2018-06-23 21:35:23 -0300

    Criando a classe de controle dos forms

---
+++ ProjetoFinal/ProjetoFinal/MeuForm.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoFinal
+{
+    public class MeuForm
+    {
+        public static Form2 FormCategoria { get; set; }
+        public static Form3 FormProduto { get; set; }
+
+
+    }
+}
